9a3cda1ca7aae97185ee5d8bddcd310db2037654
saran <saranspa2002@gmail.com>
2026-06-02 12:22:36 -0500

    Added CloudTrail audit logging

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,3 +28,79 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   block_public_policy     = true
   restrict_public_buckets = true
 }
+
+# -----------------------------
+# Phase 2 - CloudTrail
+# -----------------------------
+
+data "aws_caller_identity" "current" {}
+
+# Automatically delete CloudTrail logs after 7 days
+resource "aws_s3_bucket_lifecycle_configuration" "log_retention" {
+  bucket = aws_s3_bucket.secure_bucket.id
+
+  rule {
+    id     = "delete-cloudtrail-logs-after-7-days"
+    status = "Enabled"
+
+    filter {
+      prefix = "AWSLogs/"
+    }
+
+    expiration {
+      days = 7
+    }
+
+    noncurrent_version_expiration {
+      noncurrent_days = 7
+    }
+  }
+}
+
+# Allow CloudTrail to write logs to S3
+resource "aws_s3_bucket_policy" "cloudtrail_policy" {
+  bucket = aws_s3_bucket.secure_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AWSCloudTrailAclCheck"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action   = "s3:GetBucketAcl"
+        Resource = aws_s3_bucket.secure_bucket.arn
+      },
+      {
+        Sid    = "AWSCloudTrailWrite"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action = "s3:PutObject"
+        Resource = "${aws_s3_bucket.secure_bucket.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
+
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# Low-cost CloudTrail
+resource "aws_cloudtrail" "security_trail" {
+  name                          = "security-trail"
+  s3_bucket_name                = aws_s3_bucket.secure_bucket.id
+  include_global_service_events = true
+  is_multi_region_trail         = false
+  enable_logging                = true
+
+  depends_on = [
+    aws_s3_bucket_policy.cloudtrail_policy
+  ]
+}
